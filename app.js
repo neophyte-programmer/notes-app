8b60bdbf58a5c70fcac3a224f1f9e4c4c2532ec1
Nutifafa Attor <attorfafa@gmail.com>
2022-06-01 16:35:01 +0000

    show popup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Variable // Query Selectors
+const addBox = document.querySelector('.add__box');
+const popupBox = document.querySelector('.popup__box');
 
 // Event Listeners
+
+// Show Popup
+addBox.addEventListener('click', () => {
+    popupBox.classList.toggle('show__popup');
+});
 
 // Functions
